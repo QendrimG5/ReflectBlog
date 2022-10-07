20c56a80c776ce6a746e117ce6375b4d1ac816e6
eddieh03 <edonehaziri@gmail.com>
2022-10-07 16:43:04 +0200

    Update BlogDbContext.cs

--- Blog/Blog/Data/BlogDbContext.cs
+++ Blog/Blog/Data/BlogDbContext.cs
@@ -10,5 +10,26 @@ namespace Blog.Data
 
         public DbSet<Article> Articles => Set<Article>();
         public DbSet<User> Users => Set<User>();
+        public DbSet<Category> Categories => Set<Category>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.ApplyConfiguration(new UserConfiguration());
+            //modelBuilder.ApplyConfiguration(new ArticleConfiguration());
+
+            modelBuilder.Entity<User>()
+                        .HasMany(c => c.Articles)
+                        .WithOne(e => e.User)
+                        .OnDelete(DeleteBehavior.SetNull);
+
+            modelBuilder.Entity<Article>()
+                        .HasOne(e => e.User)
+                        .WithMany(c => c.Articles);
+
+            modelBuilder.Entity<Category>()
+                        .HasMany(c => c.Articles)
+                        .WithOne(e => e.Category)
+                        .OnDelete(DeleteBehavior.SetNull);
+        }
     }
 }
