7e5a76676ee0c76bb137da36ab6ea31c12b86e37
eddieh03 <edonehaziri@gmail.com>
2022-10-07 16:53:55 +0200

    Created files
    Created Category.cs
    Created PagedInfo.cs
    Created UserLogin.cs

---
+++ Blog/Blog/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Blog.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsDefault { get; set; }
+
+        public ICollection<Article> Articles { get; set; }
+    }
+}
---
+++ Blog/Blog/Models/PagedInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Blog.Models
+{
+    public class PagedInfo<T> where T : class
+    {
+        public List<T> Data { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
+    }
+}
