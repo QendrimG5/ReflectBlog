5e30e0a8e00a1341e517d4a0b0543770c591a8fc
eddieh03 <edonehaziri@gmail.com>
2022-10-07 16:41:01 +0200

    Create LoginController.cs

---
+++ Blog/Blog/Controllers/LoginController.cs
@@ -0,0 +1,98 @@
+﻿using Blog.Data;
+using Blog.Entities;
+using Blog.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Blog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class LoginController : ControllerBase
+    {
+        //IConf. is used to read from appsettings
+        private IConfiguration _config;
+        private readonly BlogDbContext _dbContext;
+
+        public LoginController(IConfiguration config, BlogDbContext dbContext)
+        {
+            _config = config;
+            _dbContext = dbContext;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login([FromBody] UserLogin userLogin)
+        {
+            var user = Authenticate(userLogin);
+
+            if (user != null)
+            {
+                var token = Generate(user);
+                return Ok(token);
+            }
+
+            return NotFound("User not found");
+        }
+
+        private string Generate(User user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Username),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.GivenName, user.GivenName),
+                new Claim(ClaimTypes.Surname, user.FamilyName),
+                new Claim(ClaimTypes.Role, user.Role)
+            };
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+              _config["Jwt:Audience"],
+              claims,
+              expires: DateTime.Now.AddMinutes(60),
+              signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private User Authenticate(UserLogin userLogin)
+        {
+
+            var currentUser = _dbContext.Users.FirstOrDefault(o => o.Username.ToLower() == userLogin.Username);
+
+            if (currentUser != null)
+            {
+                var createdHash = CreateMD5(currentUser.Salt + userLogin.Password);
+
+                if (createdHash.ToLower() == currentUser.Password)
+                    return currentUser;
+            }
+
+            return null;
+        }
+
+        [NonAction]
+        public string CreateMD5(string input)
+        {
+            using (MD5 md5 = MD5.Create())
+            {
+                byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                return Convert.ToHexString(hashBytes);
+            }
+        }
+    }
+}
