95b02b715c232d698211aefac939dba1cc15df1f
eddieh03 <edonehaziri@gmail.com>
2022-10-07 16:56:33 +0200

    Updates in Program.cs and appsettings.json

--- Blog/Blog/Program.cs
+++ Blog/Blog/Program.cs
@@ -1,36 +1,46 @@
 using Blog.Data;
 using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers()
-    .AddJsonOptions(x =>
-                x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 ConfigurationManager configuration = builder.Configuration;
 
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("ReactDomain",
-        policy => policy.WithOrigins("http://localhost:3000")
-            .AllowAnyHeader()
-            .AllowAnyHeader()
-            .AllowAnyOrigin()
-    );
-});
-
-
 var connString = configuration.GetConnectionString("DefaultConnection");
 
-
 builder.Services.AddDbContext<BlogDbContext>(options => options.UseSqlServer(connString));
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = configuration["Jwt:Issuer"],
+                        ValidAudience = configuration["Jwt:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]))
+                    };
+                });
+//builder.Services.AddMvc();
+
+builder.Services.AddControllers().AddJsonOptions(x =>
+                x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+//builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
@@ -39,13 +49,30 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    //app.UseDeveloperExceptionPage();
 }
+//else
+//{
+//    app.UseExceptionHandler("/Error");
+//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//    app.UseHsts();
+//}
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 
-app.UseCors("ReactDomain");
+app.UseRouting();
+
+app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.MapControllers();
+
+//app.UseEndpoints(endpoints =>
+//{
+//    endpoints.MapControllers();
+//    //endpoints.MapRazorPages();
+//});
 
 app.Run();
