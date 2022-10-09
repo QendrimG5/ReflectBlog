dd54b0b3915db23859e8e6b6a8a3a1ab5e5689aa
FlorentineDermaku <florentine.dermaku@life.gjirafa.com>
2022-10-09 17:18:41 +0200

    Update Menus.js

--- BlogFront/src/components/Menus.js
+++ BlogFront/src/components/Menus.js
@@ -3,5 +3,4 @@ export const Icons = [
   { name: "logo-twitter", link: "#" },
   { name: "logo-github", link: "#" },
   { name: "logo-linkedin", link: "#" },
-  { name: "logo-instagram", link: "#" },
 ];
