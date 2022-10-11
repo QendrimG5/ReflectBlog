04b7d52e94bdf7a32f77b2d813803d670d6737da
eddieh03 <edonehaziri@gmail.com>
2022-10-12 00:08:33 +0200

    Added Image in PostUser
    The images will be uploaded in my personal acc in Imgur

--- Blog/Blog/Controllers/ArticleController.cs
+++ Blog/Blog/Controllers/ArticleController.cs
@@ -2,11 +2,20 @@ using Blog.Data;
 using Blog.Entities;
 using Blog.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.IO;
 using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Linq.Expressions;
+using Blog.Helpers;
 
 namespace Blog.Controllers
 {
@@ -23,12 +32,18 @@ namespace Blog.Controllers
             _dbContext = dbContext;
         }
 
+        [AllowAnonymous]
         [HttpGet("GetArticles")]
-        public async Task<IActionResult> GetArticles(int page = 1, int pageSize = 10)
+        public async Task<IActionResult> GetArticles(string search, int page = 1, int pageSize = 10)
         {
+            Expression<Func<Article, bool>> searchCondition = x => x.Title.Contains(search) || x.Content.Contains(search);
+
+            var articles = await _dbContext.Articles.WhereIf(!string.IsNullOrEmpty(search), searchCondition)
+                                                   .ToListAsync();
+
             var articlesPaged = new PagedInfo<Article>
             {
-                Data = await _dbContext.Articles.Skip((page - 1) * pageSize).Take(pageSize).ToListAsync(),
+                Data = articles.Skip((page - 1) * pageSize).Take(pageSize).ToList(),
                 TotalCount = await _dbContext.Articles.CountAsync(),
                 PageSize = pageSize
             };
@@ -44,7 +59,7 @@ namespace Blog.Controllers
             return Ok(article);
         }
 
-         [HttpGet("SearchArticle")]
+        [HttpGet("SearchArticle")]
         public async Task<IEnumerable<Article>> Search(string searchText)
         {
             IQueryable<Article> query = _dbContext.Articles;
@@ -52,11 +67,12 @@ namespace Blog.Controllers
             if (!string.IsNullOrEmpty(searchText))
             {
                 query = query.Where(e => e.Title.Contains(searchText)
-                            || e.Content.Contains(searchText) );
+                            || e.Content.Contains(searchText));
             }
             return await query.ToListAsync();
         }
 
+        // This method to be called if image was already uploaded using UploadImage Endpoint
         [HttpPost("PostArticle")]
         public async Task<IActionResult> PostArticle(Article articleModel)
         {
@@ -64,6 +80,91 @@ namespace Blog.Controllers
             await _dbContext.SaveChangesAsync();
 
             return Ok(articleToAdd.Entity);
+        }
+
+        // This method to be called if you want to create article and upload image in the same time
+        [HttpPost("PostArticleV1")]
+        public async Task<IActionResult> PostArticleV1([FromForm]ArticleModel articleModel)
+        {
+            var article = new Article
+            {
+                Title = articleModel.Title,
+                Content = articleModel.Content,
+                Date = articleModel.Date,
+                AuthorId = articleModel.AuthorId,
+                CategoryId = articleModel.CategoryId
+            };
+
+            article.ImageUrl = await UploadImageToCdn(articleModel.Image);
+
+            var articleToAdd = await _dbContext.AddAsync(articleModel);
+            await _dbContext.SaveChangesAsync();
+
+            return Ok(articleToAdd.Entity);
+        }
+
+        ///Endpoint to upload picture
+        [AllowAnonymous]
+        [HttpPost("UploadImage")]
+        //[NonAction]
+        public async Task<IActionResult> UploadImage(IFormFile image)
+        {
+            var extension = Path.GetExtension(image.FileName);
+
+            if (extension != ".png")
+            {
+                return BadRequest("Only png files are accepted.");
+            }
+
+            string base64Image;
+            using (var ms = new MemoryStream())
+            {
+                image.CopyTo(ms);
+                var fileBytes = ms.ToArray();
+                base64Image = Convert.ToBase64String(fileBytes);
+                // act on the Base64 data
+            }
+
+            var httpclient = new HttpClient();
+            httpclient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Client-ID", "c88b8634c974be4");
+            httpclient.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "text/plain");
+            var response = await httpclient.PostAsync("https://api.imgur.com/3/image", new StringContent(base64Image));
+            var stringcontent = await response.Content.ReadAsStringAsync();
+            var imgurResponseModel = JsonConvert.DeserializeObject<ImgurResponseModel>(stringcontent);
+
+            //return ImgurResponseModel.Data.Link;
+            return Ok(imgurResponseModel.Data.Link);
+        }
+
+        // method to upload picture if V1 of post was used
+        [NonAction]
+        public async Task<string> UploadImageToCdn(IFormFile image)
+        {
+            var extension = Path.GetExtension(image.FileName);
+
+            if (extension != ".png")
+            {
+                return null;
+            }
+
+            string base64Image;
+            using (var ms = new MemoryStream())
+            {
+                image.CopyTo(ms);
+                var fileBytes = ms.ToArray();
+                base64Image = Convert.ToBase64String(fileBytes);
+                // act on the Base64 data
+            }
+
+            var httpclient = new HttpClient();
+            httpclient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Client-ID", "a65e2cd7035148f");
+            httpclient.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "text/plain");
+            var response = await httpclient.PostAsync("https://api.imgur.com/3/image", new StringContent(base64Image));
+            var stringcontent = await response.Content.ReadAsStringAsync();
+            var imgurResponseModel = JsonConvert.DeserializeObject<ImgurResponseModel>(stringcontent);
+
+            //return ImgurResponseModel.Data.Link;
+            return imgurResponseModel.Data.Link;
         }
 
         [HttpPut("UpdateArticle")]
---
+++ Blog/Blog/Helpers/SearchHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+using System.Linq;
+using System;
+
+namespace Blog.Helpers
+{
+    public static class SearchHelper
+    {
+        public static IQueryable<T> WhereIf<T>(this IQueryable<T> query, bool condition, Expression<Func<T, bool>> predicate)
+        {
+            return condition
+                ? query.Where(predicate)
+                : query;
+        }
+    }
+}
