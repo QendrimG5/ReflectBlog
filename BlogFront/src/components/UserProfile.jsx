27e0792ac4272b395801d643aa42fc739a6860f2
MuhammedSmani <muhammedsmani11@gmail.com>
2022-10-11 21:08:07 +0200

    UserProfile User Icon rounded fixed

--- BlogFront/src/components/UserProfile.jsx
+++ BlogFront/src/components/UserProfile.jsx
@@ -8,8 +8,10 @@ const UserProfile = () => {
     <div>
       <div>
         <div className="flex lg:flex-row flex-col justify-between items-center pb-20 gap-10">
-          <div className='lg:w-1/4'>
-            <img src={userImg} alt="" className='rounded-full min-w-[100px] w-[300px]' />
+          <div className='lg:w-1/4 w-full  flex justify-center'>
+            <div className='min-w-[100px] w-[200px] min-h-[100px] h-[200px]'>
+              <img src={userImg} alt="" className='w-full h-full rounded-full' />
+            </div>
           </div>
           <div className='lg:w-3/4 w-full grid lg:grid-cols-2 grid-cols-1 gap-x-5'>
             <div className="pb-4">
@@ -20,14 +22,14 @@ const UserProfile = () => {
               <label className="tex-lg text-gray-800" >Last Name:    </label>
               <input className="w-full border-2 rounded-xl p-4 mt-1 bg-primary border-transparent  focus:border-secondary focus:ring-secondary transition ease-in-out focus:outline-none font-light text-gray-400" placeholder="Smani" />
             </div>
-          <div className="pb-4">
-            <label className="tex-lg text-gray-800" > User Name:    </label>
-            <input className="w-full border-2 rounded-xl p-4 mt-1 bg-primary border-transparent  focus:border-secondary focus:ring-secondary transition ease-in-out focus:outline-none font-light text-gray-400" placeholder="muhammedsmani" />
-          </div>
-          <div className="pb-4">
-            <label className="tex-lg text-gray-800" >Email:    </label>
-            <input className="w-full border-2 rounded-xl p-4 mt-1 bg-primary border-transparent  focus:border-secondary focus:ring-secondary transition ease-in-out focus:outline-none font-light text-gray-400" placeholder="muhammed.smani@life.gjirafa.com" />
-          </div>
+            <div className="pb-4">
+              <label className="tex-lg text-gray-800" > User Name:    </label>
+              <input className="w-full border-2 rounded-xl p-4 mt-1 bg-primary border-transparent  focus:border-secondary focus:ring-secondary transition ease-in-out focus:outline-none font-light text-gray-400" placeholder="muhammedsmani" />
+            </div>
+            <div className="pb-4">
+              <label className="tex-lg text-gray-800" >Email:    </label>
+              <input className="w-full border-2 rounded-xl p-4 mt-1 bg-primary border-transparent  focus:border-secondary focus:ring-secondary transition ease-in-out focus:outline-none font-light text-gray-400" placeholder="muhammed.smani@life.gjirafa.com" />
+            </div>
           </div>
         </div>
       </div>
