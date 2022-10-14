4d50f8c45e6a194cb6bcb7eb1745209a8fcc12b9
FlorentineDermaku <florentine.dermaku@life.gjirafa.com>
2022-10-14 21:57:15 +0200

    Update PostSuggestions.jsx

--- BlogFront/src/components/PostSuggestions.jsx
+++ BlogFront/src/components/PostSuggestions.jsx
@@ -6,7 +6,7 @@ import { postOne, userImg } from '../assets';
 const PostSuggestions = () => {
     return (
         <div className='pt-20'>
-            <h2 className='mb-5 lg:text-3xl md:text-2xl sm:text-xl text-lg font-medium tracking-tight text-gray-800 bg-primary px-4 py-2 rounded-lg'>Read other social posts</h2>
+            <h2 className='mb-6 lg:text-2xl md:text-2xl sm:text-xl text-lg font-medium tracking-normal text-gray-800 bg-primary px-5 py-3 rounded-lg'>Read other social posts</h2>
             <div className='grid xl:grid-cols-4 lg:grid-cols-2 md:grid-cols-2 grid-cols-1 gap-x-20 gap-y-10'>
                 <PostCard postImg={postOne} category="Social" title="Reflect! The brand new blog website." userImg={userImg} user="Muhammed Smani" date="6 Oct 2022" />
                 <PostCard postImg={postOne} category="Social" title="Reflect! The brand new blog website." userImg={userImg} user="Muhammed Smani" date="6 Oct 2022" />
