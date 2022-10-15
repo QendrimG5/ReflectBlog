c0955b266dc89ee45f6e0b93c5fd7091e25eb10e
eddieh03 <edonehaziri@gmail.com>
2022-10-15 20:23:51 +0200

    LogIn&SignUp Form Changes

--- BlogFront/src/components/LogInForm.jsx
+++ BlogFront/src/components/LogInForm.jsx
@@ -3,6 +3,7 @@ import * as React from "react";
 import { useRef } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
+import { ReflectDarkBlueWhite } from "../assets";
 
 export default function LogInForm() {
 
@@ -36,37 +37,37 @@ export default function LogInForm() {
 
 
 
-        <div className="bg-primary px-10 py-20 rounded-3xl w-[500px]">
-            <h1 className="text-4xl font-semibold text-gray-800">Welcome!</h1>
-            <p className="text-lg text-gray-400 mt-2"> Please enter your details.</p>
-            <div className="mt-8 ">
+        <div className="bg-primary/70 backdrop-blur-md px-8 py-12 rounded-3xl w-[500px]">
+            <h1 className="md:text-3xl text-2xl font-semibold text-gray-800 pb-2">Welcome!</h1>
+            <p className="md:text-lg text-base text-gray-400 pb-8"> Please enter your details.</p>
+            <div className="">
                 <div className="pb-4">
-                    <label className="tex-lg text-gray-800" >Username:    </label>
-                    <input className="w-full border-2 rounded-xl p-4 mt-1 bg-white border-transparent  focus:border-secondary focus:ring-secondary transition ease-in-out focus:outline-none font-light text-gray-400" placeholder="Enter your email" ref={Username} />
+                    <label className="md:text-lg text-base text-gray-800" >Username:    </label>
+                    <input className="w-full border-2 rounded-xl p-4 mt-2 bg-white border-transparent  focus:border-secondary focus:ring-secondary transition ease-in-out focus:outline-none font-light text-gray-400 md:text-lg text-base" placeholder="Enter your email" ref={Username} />
                 </div>
-                <div className="pb-8">
-                    <label type="password" className="tex-lg text-gray-800" >Password:    </label>
-                    <input className="w-full border-2 rounded-xl p-4 mt-1 bg-white border-transparent  focus:border-secondary focus:ring-secondary transition ease-in-out focus:outline-none font-light text-gray-400" placeholder="Enter your password" ref={Password} />
+                <div className="pb-4">
+                    <label type="password" className="md:text-lg text-base text-gray-800" >Password:    </label>
+                    <input className="w-full border-2 rounded-xl p-4 mt-2 bg-white border-transparent  focus:border-secondary focus:ring-secondary transition ease-in-out focus:outline-none font-light text-gray-400 md:text-lg text-base" placeholder="Enter your password" ref={Password} />
                 </div>
-                <div className="pb-12 flex justify-between items-center">
+                <div className="pb-8 flex justify-between items-center">
                     <div className="flex items-center">
                         <input
                             type="checkbox"
                             id="remember"
-                            class="w-5 h-5 focus:ring-2"/>
-                        <label className="ml-2 font-normal text-base text-gray-800" for="remember"> Remember Me</label>
+                            class="w-4 h-4"/>
+                        <label className="ml-2 font-normal md:text-lg text-base text-gray-800" for="remember">Remember Me</label>
                     </div>
                     <div>
-                        <button className="font-medium text-base text-secondary "> Forgot password</button>
+                        <button className="font-medium md:text-lg text-base text-secondary ">Forgot password</button>
                     </div>
                 </div>
-                <div className="flex flex-col gap-y-4">
-                    <button className="active:scale-[.98] active:duration-75 hover:scale-[1.05] ease-in-out transition-all py-3 rounded-xl bg-secondary text-white text-lg font-bold" onClick={addUserHandler}>Log In</button>
+                <div className="flex flex-col gap-y-4 pb-8">
+                    <button className="active:scale-[.98] active:duration-75 hover:scale-[1.05] ease-in-out transition-all py-3 rounded-xl bg-secondary text-white md:text-lg text-base font-bold" onClick={addUserHandler}>Log In</button>
                 </div>
-                <div className="mt-8 flex justify-center item-center">
-                    <p className="font-normal text-base text-gray-800">Don't have an account?</p>
+                <div className=" flex justify-center item-center">
+                    <p className="font-normal md:text-lg text-base text-gray-800">Don't have an account?</p>
                     <Link to="/sign-up">
-                        <button className="text-secondary text-base font-medium ml-2">Sign Up</button>
+                        <button className="text-secondary md:text-lg text-base font-medium ml-2">Sign Up</button>
                     </Link>
                 </div>
             </div>
--- BlogFront/src/components/SignUpForm.jsx
+++ BlogFront/src/components/SignUpForm.jsx
@@ -1,77 +1,74 @@
 import * as React from "react";
+
 import { useRef } from "react";
-import axios from "axios"
 import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { ReflectDarkBlueWhite } from "../assets";
 
-export default function SignUpForm() {
+export default function LogInForm() {
 
+    const navigate = useNavigate();
     const Username = useRef("");
     const Password = useRef("");
-    const Email = useRef("");
 
-    const GivenName = useRef("");
-    const FamilyName = useRef("");
-    const navigate = useNavigate();
 
     const navigateHome = () => {
         // 👇️ navigate to /
         navigate('/');
     };
-
     async function addUserHandler() {
         var data = {
 
 
             username: Username.current.value,
-            salt: '',
             password: Password.current.value,
-            email: Email.current.value,
-            role: '',
-            givenName: GivenName.current.value,
-            familyName: FamilyName.current.value,
-
 
         }
 
-        await axios.post("https://localhost:7244/api/User/PostUser", data)
+        await axios.post("https://localhost:7244/api/Login", data)
             .then((response) => {
                 navigateHome();
 
+                window.localStorage.setItem("token", response.data)
+
             })
     }
-
     return (
-        <div className="bg-primary px-10 py-12 rounded-3xl w-[500px]">
-            <h1 className="text-4xl font-semibold text-gray-800">Welcome!</h1>
-            <p className="text-lg text-gray-400 mt-2"> Fill the form to get registered.</p>
-            <div className="mt-8 ">
-                <div className="pb-4">
-                    <label className="tex-lg text-gray-800" >Email:    </label>
-                    <input className="w-full border-2 rounded-xl p-4 mt-1 bg-white border-transparent  focus:border-secondary focus:ring-secondary transition ease-in-out focus:outline-none font-light text-gray-400" placeholder="Enter your email" ref={Email} />
-                </div>
-                <div className="pb-4">
-                    <label className="tex-lg text-gray-800" > User Name:    </label>
-                    <input className="w-full border-2 rounded-xl p-4 mt-1 bg-white border-transparent  focus:border-secondary focus:ring-secondary transition ease-in-out focus:outline-none font-light text-gray-400" placeholder="Enter your username" ref={Username} />
-                </div>
-                <div className="pb-4">
-                    <label className="tex-lg text-gray-800" >First Name:    </label>
-                    <input className="w-full border-2 rounded-xl p-4 mt-1 bg-white border-transparent  focus:border-secondary focus:ring-secondary transition ease-in-out focus:outline-none font-light text-gray-400" placeholder="Enter your first name" ref={GivenName} />
-                </div>
-                <div className="pb-4">
-                    <label className="tex-lg text-gray-800" >Last Name:    </label>
-                    <input className="w-full border-2 rounded-xl p-4 mt-1 bg-white border-transparent  focus:border-secondary focus:ring-secondary transition ease-in-out focus:outline-none font-light text-gray-400" placeholder="Enter your last name" ref={FamilyName} />
-                </div>
-                <div className="pb-4">
-                    <label className="tex-lg text-gray-800" > Password:    </label>
-                    <input type="password" className="w-full border-2 rounded-xl p-4 mt-1 bg-white border-transparent  focus:border-secondary focus:ring-secondary transition ease-in-out focus:outline-none font-light text-gray-400" placeholder="Enter your password" ref={Password} />
-                </div>
-                {/* <div className="pb-12">
-                    <label className="tex-lg text-gray-800" >Your Birthday:    </label>
-                    <input type="date" className="w-full border-2 rounded-xl p-4 mt-1 bg-white border-transparent  focus:border-secondary focus:ring-secondary transition ease-in-out focus:outline-none font-light text-gray-400" placeholder="Your Birthday" ref={userBirthday} />
-                </div> */}
 
-                <div className="mt-8 flex flex-col gap-y-4">
-                    <button className="active:scale-[.98] active:duration-75 hover:scale-[1.05] ease-in-out transition-all py-3 rounded-xl bg-secondary text-white text-lg font-bold" onClick={addUserHandler}><Link to='/sign-in'>Register</Link></button>
+
+
+        <div className="bg-primary/70 backdrop-blur-md px-8 py-12 rounded-3xl w-[500px]">
+            <h1 className="md:text-3xl text-2xl font-semibold text-gray-800 pb-2">Welcome!</h1>
+            <p className="md:text-lg text-base text-gray-400 pb-8"> Please enter your details.</p>
+            <div className="">
+                <div className="pb-4">
+                    <label className="md:text-lg text-base text-gray-800" >Username:    </label>
+                    <input className="w-full border-2 rounded-xl p-4 mt-2 bg-white border-transparent  focus:border-secondary focus:ring-secondary transition ease-in-out focus:outline-none font-light text-gray-400 md:text-lg text-base" placeholder="Enter your email" ref={Username} />
+                </div>
+                <div className="pb-4">
+                    <label type="password" className="md:text-lg text-base text-gray-800" >Password:    </label>
+                    <input className="w-full border-2 rounded-xl p-4 mt-2 bg-white border-transparent  focus:border-secondary focus:ring-secondary transition ease-in-out focus:outline-none font-light text-gray-400 md:text-lg text-base" placeholder="Enter your password" ref={Password} />
+                </div>
+                <div className="pb-8 flex justify-between items-center">
+                    <div className="flex items-center">
+                        <input
+                            type="checkbox"
+                            id="remember"
+                            class="w-4 h-4"/>
+                        <label className="ml-2 font-normal md:text-lg text-base text-gray-800" for="remember">Remember Me</label>
+                    </div>
+                    <div>
+                        <button className="font-medium md:text-lg text-base text-secondary ">Forgot password</button>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-y-4 pb-8">
+                    <button className="active:scale-[.98] active:duration-75 hover:scale-[1.05] ease-in-out transition-all py-3 rounded-xl bg-secondary text-white md:text-lg text-base font-bold" onClick={addUserHandler}>Log In</button>
+                </div>
+                <div className=" flex justify-center item-center">
+                    <p className="font-normal md:text-lg text-base text-gray-800">Don't have an account?</p>
+                    <Link to="/sign-up">
+                        <button className="text-secondary md:text-lg text-base font-medium ml-2">Sign Up</button>
+                    </Link>
                 </div>
             </div>
         </div>
